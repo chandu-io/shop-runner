ace844344590781382b41c5fd68c4339114e4bee
deeks <deeks@192.168.200.107>
2017-05-09 20:22:12 -0400

    Make the App runnable

--- src/main/java/com/shop/runner/ShopRunnerApplication.java
+++ src/main/java/com/shop/runner/ShopRunnerApplication.java
@@ -1,12 +1,23 @@
 package com.shop.runner;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ShopRunnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopRunnerApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	String get()
+	{
+		return "Hello World!! The time is now: " + new Date();
 	}
 }
